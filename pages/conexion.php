9f284856af2ee398af5d7e63295ded4679f104b6
Belen De Franchi <belendefranchi@gmail.com>
2022-12-29 18:14:24 -0300

    agrego conexion base don web

--- pages/conexion.php
+++ pages/conexion.php
@@ -1,6 +1,7 @@
 <?php
     try{
         $base=new PDO ("mysql:host=localhost; dbname=login", "root", "");
+        //$base=new PDO ("mysql:host=localhost; dbname=c1760806_cac", "c1760806_cac", "EstupidoDonWeb4");
         $base->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $base->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $base->exec ("SET CHARACTER SET UTF8");
